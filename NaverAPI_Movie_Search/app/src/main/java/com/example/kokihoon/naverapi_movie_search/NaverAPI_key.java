6dd0dc9a523ccd3bbbcdf8698b41f53498db6b06
kokihoon <rhrlgns777@gmail.com>
2018-12-16 16:34:21 +0900

    add NaverAPI_KEY

---
+++ NaverAPI_Movie_Search/app/src/main/java/com/example/kokihoon/naverapi_movie_search/NaverAPI_key.java
@@ -0,0 +1,9 @@
+package com.example.kokihoon.naverapi_movie_search;
+
+public class NaverAPI_key {
+    public final static String clientId = "4h8XPYoAqJR2jFWefxLU";//애플리케이션 클라이언트 아이디값";
+    public final static String clientSecret = "0Hopez1sls";//애플리케이션 클라이언트 시크릿값";
+}
+
+
+
